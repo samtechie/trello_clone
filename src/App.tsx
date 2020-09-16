6887ac432bfd4fced28e2855b4dbab7b1cb7e1a0
Wasswa Samuel <latimerscope@gmail.com>
2020-09-16 21:51:29 +0200

    Initial Commit Build basic component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Column } from "./Column";
+import { Card } from "./Card";
+import { AppContainer } from "./styles";
+import { AddNewItem } from "./AddNewItem"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold"></Card>
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript"></Card>
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use Static typing"></Card>
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+    </AppContainer>
   );
 }
 
